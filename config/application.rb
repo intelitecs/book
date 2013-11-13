f84e5ade7868d679fe74b3362c1592f3cfaa924e
jarodez <jarode@me.com>
2013-11-13 12:36:00 +0100

    translation missing problem resolved

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Blog
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.i18n.default_locale = :fr
+    #config.i18n.default_locale = :fr
    
    #assets precomile for heroku
    config.assets.initialize_on_precompile = false

fe33721108209e33bb23b8b1c8e935c5d6dcee12
jarodez <jarode@me.com>
2013-11-13 12:41:32 +0100

    user change email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,8 @@ class User < ActiveRecord::Base
   validates :username, :email, uniqueness: :true
   validates :username, length: {minimum: 6, maximum: 15}
   validates :password, length: {minimum: 7, maximum: 21}
-  EmailRegex = /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{3,63}\.[a-zA-Z]{2,6})\z/i
-  validates :email, format: {with: self::EmailRegex}
+  #EmailRegex = /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{3,63}\.[a-zA-Z]{2,6})\z/i
+  #validates :email, format: {with: self::EmailRegex}
   has_many :comments, dependent: :destroy
   has_many :articles, through: :comments
 
